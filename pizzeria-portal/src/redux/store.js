94a81049ac95a07484969cd1f1f36501345c0a38
Kardianis <michal_kuszneruk@tlen.pl>
2021-04-13 22:42:13 +0200

    wszystko

---
+++ pizzeria-portal/src/redux/store.js
@@ -0,0 +1,60 @@
+import {combineReducers, createStore, applyMiddleware} from 'redux';
+import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
+
+import tablesReducer from './tablesRedux';
+import bookingReducer from './bookingRedux';
+import dishesReducer from './kitchenRedux';
+
+// define initial state and shallow-merge initial data
+const initialState = {
+  tables: {
+    data: {},
+    loading: {
+      active: false,
+      error: false,
+    },
+  },
+  bookings: {
+    data: {},
+    loading: {
+      active: false,
+      error: false,
+    },
+  },
+
+  dishes: {
+    data: {},
+    loading: {
+      active: false,
+      error: false,
+    },
+  },
+};
+
+// define reducers
+const reducers = {
+  tables: tablesReducer,
+  bookings: bookingReducer,
+  dishes: dishesReducer,
+};
+
+// add blank reducers for initial state properties without reducers
+Object.keys(initialState).forEach(item => {
+  if (typeof reducers[item] == 'undefined') {
+    reducers[item] = (statePart = null) => statePart;
+  }
+});
+
+const combinedReducers = combineReducers(reducers);
+
+// create store
+const store = createStore(
+  combinedReducers,
+  initialState,
+  composeWithDevTools(
+    applyMiddleware(thunk)
+  )
+);
+
+export default store;
