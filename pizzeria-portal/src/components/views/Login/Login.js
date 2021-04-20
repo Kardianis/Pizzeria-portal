d40162ff5caea149c8d57bd8fcde0f885990ee93
Kardianis <michal_kuszneruk@tlen.pl>
2021-04-20 12:19:04 +0200

    main

--- pizzeria-portal/src/components/views/Login/Login.js
+++ pizzeria-portal/src/components/views/Login/Login.js
@@ -1,12 +1,120 @@
 import React from 'react';
-import styles from './Login.scss';
+import {
+  Button,
+  TextField,
+  Grid,
+  Paper,
+  Typography,
+  Link,
+} from '@material-ui/core';
 import PropTypes from 'prop-types';
+import styles from './Login.module.scss';
 
-const Login = () => {
-return(
-    <div className={styles.component}>
-        <h2>Login view</h2>
-    </div>
-)};
+
+class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { username: '', password:'', authflag:1 };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  static propTypes = {
+    history: PropTypes.node,
+  }
+
+  handleChange(event) {
+    this.setState({ username: event.state.username, password: event.state.password });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    this.props.history.push('/panel');
+  }
+  
+  render() {
+    return (
+      <div>
+        <Grid container spacing={0} justify="center" direction="row">
+          <Grid item>
+            <Grid
+              container
+              direction="column"
+              justify="center"
+              spacing={2}
+              className={styles.loginForm}
+            >
+              <Paper
+                variant="elevation"
+                elevation={2}
+                className={styles.loginBackground}
+              >
+                <Grid item>
+                  <Typography component="h1" variant="h5">
+Sign in
+                  </Typography>
+                </Grid>
+                <Grid item>
+                  <form onSubmit={this.handleSubmit}>
+                    <Grid container direction="column" spacing={2}>
+                      <Grid item>
+                        <TextField
+                          type="email"
+                          placeholder="Email"
+                          fullWidth
+                          name="username"
+                          variant="outlined"
+                          value={this.state.username}
+                          onChange={(event) =>
+                            this.setState({
+                              [event.target.name]: event.target.value,
+                            })
+                          }
+                          required
+                          autoFocus
+                        />
+                      </Grid>
+                      <Grid item>
+                        <TextField
+                          type="password"
+                          placeholder="Password"
+                          fullWidth
+                          name="password"
+                          variant="outlined"
+                          value={this.state.password}
+                          onChange={(event) =>
+                            this.setState({
+                              [event.target.name]: event.target.value,
+                            })
+                          }
+                          required
+                        />
+                      </Grid>
+                      <Grid item>
+                        <Button
+                          variant="contained"
+                          color="primary"
+                          type="submit"
+                          className={styles.buttonBlock}
+                        >
+Submit
+                        </Button>
+                      </Grid>
+                    </Grid>
+                  </form>
+                </Grid>
+                <Grid item>
+                  <Link href="#" variant="body2">
+Forgot Password?
+                  </Link>
+                </Grid>
+              </Paper>
+            </Grid>
+          </Grid>
+        </Grid>
+      </div>
+    );
+  }
+}
 
 export default Login;
---
+++ pizzeria-portal/src/components/views/Waiter/Waiter.js
@@ -0,0 +1,159 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './Waiter.module.scss';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+import Button from '@material-ui/core/Button';
+
+class Waiter extends React.Component {
+  static propTypes = {
+    fetchTables: PropTypes.func,
+    changeTableStatus: PropTypes.func,
+    tables: PropTypes.array,
+    loading: PropTypes.shape({
+      active: PropTypes.bool,
+      error: PropTypes.node,
+    }),
+  }
+
+  componentDidMount(){
+    const { fetchTables } = this.props;
+    fetchTables();
+  }
+
+  renderStatus(id, status, order){
+    const {changeTableStatus} = this.props;
+    switch (status) {
+      case 'free':
+        status = 'thinking';
+        order = order;
+        changeTableStatus(id, status, order);
+        break;      
+      case 'thinking':
+        status = 'ordered';
+        order = order;
+        changeTableStatus(id, status, order);
+        break;
+      case 'ordered':
+        status = 'prepared';
+        order = order;
+        changeTableStatus(id, status, order);
+        break;
+      case 'prepared':
+        status = 'delivered';
+        order = order;
+        changeTableStatus(id, status, order);
+        break;
+      case 'delivered':
+        status = 'paid';
+        order = order;
+        changeTableStatus(id, status, order);
+        break;
+      case 'paid':
+        break;
+      default:
+        return null;
+    }
+  }
+
+  renderActions(row){
+    let id  = row.id;
+    let status = row.status;
+    let order = row.order;
+
+    switch (status) {
+      case 'free':
+        return (
+          <>
+            <Button onClick={() => this.renderStatus(id, status, order)}>thinking</Button>
+            <Button onClick={() => this.renderStatus(id, status, order)}>new order</Button>
+          </>
+        );
+      case 'thinking':
+        return (
+          <Button onClick={() => this.renderStatus(id, status, order)}>new order</Button>
+        );
+      case 'ordered':
+        return (
+          <Button onClick={() => this.renderStatus(id, status, order)}>prepared</Button>
+        );
+      case 'prepared':
+        return (
+          <Button onClick={() => this.renderStatus(id, status, order)}>delivered</Button>
+        );
+      case 'delivered':
+        return (
+          <Button onClick={() => this.renderStatus(id, status, order)}>paid</Button>
+
+        );
+      case 'paid':
+        return (
+          <Button onClick={() => this.renderStatus(id, status, order)}>free</Button>
+        );
+      default:
+        return null;
+    }
+  }
+
+  render() {
+    const { loading: { active, error }, tables, changeTableStatus } = this.props;
+
+    if(active || !tables.length){
+      return (
+        <Paper className={styles.component}>
+          <p>Loading...</p>
+        </Paper>
+      );
+    } else if(error) {
+      return (
+        <Paper className={styles.component}>
+          <p>Error! Details:</p>
+          <pre>{error}</pre>
+        </Paper>
+      );
+    } else {
+      return (
+        <Paper className={styles.component}>
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell>Table</TableCell>
+                <TableCell>Status</TableCell>
+                <TableCell>Order</TableCell>
+                <TableCell>Action</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {tables.map(row => (
+                <TableRow key={row.id}>
+                  <TableCell component="th" scope="row">
+                    {row.id}
+                  </TableCell>
+                  <TableCell>
+                    {row.status}
+                  </TableCell>
+                  <TableCell>
+                    {row.order && (
+                      <Button to={`${process.env.PUBLIC_URL}/waiter/order/${row.order}`}>
+                        {row.order}
+                      </Button>
+                    )}
+                  </TableCell>
+                  <TableCell>
+                    {this.renderActions(row)}
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </Paper>
+      );
+    }
+  }
+}
+
+export default Waiter;
